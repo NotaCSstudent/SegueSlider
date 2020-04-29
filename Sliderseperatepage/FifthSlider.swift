913782cd8160009358da3b85bfa85acd89a41722
Mahamud Babu <mahmudbabu94@gmail.com>
2020-04-29 12:29:46 -0400

    Added 5 sliders and 5 segues

---
+++ Sliderseperatepage/FifthSlider.swift
@@ -0,0 +1,30 @@
+//
+//  FifthSlider.swift
+//  Sliderseperatepage
+//
+//  Created by admin on 4/29/20.
+//  Copyright © 2020 Mbabu. All rights reserved.
+//
+
+import UIKit
+
+class FifthSlider: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Sliderseperatepage/ViewController.swift
+++ Sliderseperatepage/ViewController.swift
@@ -9,12 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    ///------------------------------------First slider
+    @IBOutlet weak var sliderlabel: UILabel!
+    @IBOutlet weak var Slider1: UISlider!
+    //------------------------------------------
+    
+    
+    
+    ///------------------------------------Second slider
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //-------------------------------------
+    
+    
+    
+    //----------------------------Third Slider
+    
+    //------------------------------------------
+    
+    
+    
+    
+    //-------------------------------------Fourth Slider
+    
+    
+    //---------------------------------------------------
+    
+    
+    //----------------------------------------Fifth slider
+    
+    //--------------------------------------------------------------
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    @IBAction func Sliders(_ sender: UISlider) {
+        
+        sliderlabel.text = String(Slider1.value)
+        
+        
+    }
+    
 
 }
 
